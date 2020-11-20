c1abf27095ff0dd4577e0460369e812d2c86cc35
DanZama <33463552+DanZama@users.noreply.github.com>
2020-11-20 16:28:04 -0600

    Update ServidorRMI.java

--- ServidorRMI.java
+++ ServidorRMI.java
@@ -1,13 +1,12 @@
 import java.rmi.Naming;
 import java.lang.Exception;
 
-
 public class ServidorRMI{
     public static void main(String[] args) throws Exception{
         String url = "rmi://localhost/multiplicacion";
         url = "rmi://" + args[0] + "/multiplicacion";
         ClaseRMI obj = new ClaseRMI();
         Naming.bind(url,obj);
-        System.out.println("Servidor "+ args[0] +" iniciado con exito!");
+        System.out.println("Servidor "+ args[0]);
     }
 }
